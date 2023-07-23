26cc9e21fb1c3bc942d47788afc3741fb3108fe7
jurco-exe <jurajbosy1@gmail.com>
2023-07-23 21:10:12 +0200

    feat: useState is almost done only thing that each use-case i need to make as a new component

--- src/app/hooks/use-state/page.tsx
+++ src/app/hooks/use-state/page.tsx
@@ -5,44 +5,119 @@ import Image from 'next/image';
 
 import { useState } from 'react';
 
+import todoData from '@/data/todo-data.json';
+
+import Counter from '@/components/useState/Counter';
+
 type Props = {};
 
 const UseState = (props: Props) => {
 	const [count, setCount] = useState<number>(0);
 
+	const [user, setUser] = useState({ isAdmin: true });
+
+	const [name, setName] = useState<string>();
+	const [email, setEmail] = useState<string>();
+	const [password, setPassword] = useState<number>();
+	const [gender, setGender] = useState<string>();
+	const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
+	const handleSubmit = () => {
+		event?.preventDefault();
+
+		setIsSubmitting(true);
+		setTimeout(() => {
+			setIsSubmitting(false);
+			alert('Form submission successful');
+		}, 3000);
+	};
+
+	const [todoList, setTodoList] = useState(todoData);
+
 	return (
 		<>
-			<section className="grid grid-cols-2 gap-5">
-				<div className=" col-start-1 col-end-2 border border-grey-main rounded-lg p-5 flex gap-5 flex-col">
-					<h1 className="text-2xl">1. Kepping Track of the previous state</h1>
-					<h1 className="mb-3">
-						Count:{' '}
-						<span className=" text-blue-main border border-blue-main py-1 px-2 rounded-lg bg-secondary">
-							{count}
-						</span>
-					</h1>
-					<div className="flex gap-5">
-						<button
-							onClick={() => setCount(count + 1)}
-							className="border rounded-lg py-1 px-3"
-						>
-							Icrement
-						</button>
-						<button
-							onClick={() => setCount(count - 1)}
-							className="border rounded-lg py-1 px-3"
-						>
-							Decrement
-						</button>
+			<section className="grid grid-cols-2 grid-rows-3 gap-5">
+				<Counter />
+				<div className="col-start-2 col-end-3 border border-grey-main rounded-lg p-5 flex gap-5 flex-col">
+					<h1 className="text-2xl">2. Conditional Rendering</h1>
+					<div>
+						{user.isAdmin ? (
+							<h1 className="mb-5">Admin: TRUE</h1>
+						) : (
+							<h1 className="mb-5">Admin: FALSE</h1>
+						)}
+						<table className="flex flex-col gap-2">
+							<tr className="flex gap-2">
+								<th>id</th>
+								<th>Name</th>
+								{user.isAdmin && <th>Action</th>}
+							</tr>
+							<tr className="flex gap-3">
+								<td>1.</td>
+								<td>Juraj</td>
+								{user.isAdmin && (
+									<td>
+										<button className="border rounded-lg px-2 bg-orange-main border-white text-white">
+											Edit
+										</button>
+									</td>
+								)}
+							</tr>
+						</table>
 					</div>
-					<Image
-						src={'/react-useState.svg'}
-						alt={''}
-						width={700}
-						height={300}
-					></Image>
 				</div>
-				<div className="col-start-2 col-end-3 border p-5 flex gap-5 flex-col"></div>
+				<div className="col-start-1 col-end-2 border border-grey-main rounded-lg p-5 flex gap-5 flex-col">
+					<h1 className="text-2xl">3. Form Handling</h1>
+					<div>
+						<form onSubmit={handleSubmit} className="flex flex-col gap-2">
+							<input
+								type="text"
+								placeholder="Enter name"
+								onChange={(e) => {
+									setName(e.target.value);
+								}}
+								value={name}
+								required
+								className="w-72 py-1 px-2 rounded-lg bg-bg-secondary"
+							/>
+							<input
+								type="email"
+								placeholder="Enter email"
+								onChange={(e) => {
+									setEmail(e.target.value);
+								}}
+								value={email}
+								required
+								className="w-72 py-1 px-2 rounded-lg bg-bg-secondary"
+							/>
+							<label>Gender</label>
+							<select
+								onChange={(e) => {
+									setGender(e.target.value);
+								}}
+								value={gender}
+								required
+								className="w-72 py-1 px-2 rounded-lg text-black"
+							>
+								<option>Male</option>
+								<option>Female</option>
+							</select>
+							<br />
+
+							<button
+								type="submit"
+								className="w-72 py-1 px-2 rounded-lg border"
+							>
+								Submit
+							</button>
+							{isSubmitting && 'Submitting...'}
+						</form>
+					</div>
+				</div>
+				<div className="col-start-2 col-end-3 border border-grey-main rounded-lg p-5 flex gap-5 flex-col">
+					<h1 className="text-2xl">4. TODO list (funcionalities)</h1>
+					<div>nehehehehe</div>
+				</div>
 			</section>
 		</>
 	);
---
+++ src/components/useState/Counter.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+
+type Props = {};
+
+const Counter = (props: Props) => {
+	const [count, setCount] = useState<number>(0);
+
+	return (
+		<div className="col-start-1 col-end-2 border border-grey-main rounded-lg p-5 flex gap-5 flex-col">
+			<h1 className="text-2xl"> 1. Keep track of the previous state</h1>
+			<div className="flex flex-col gap-3">
+				<h3 className="text-xl flex items-center gap-3">
+					Count:{' '}
+					<span className="text-sm text-blue-main border px-2 border-blue-main rounded-lg">
+						{count}
+					</span>
+				</h3>
+				<div className="flex gap-5">
+					<button
+						className="text-md border rounded-lg py-1 px-2 hover:bg-bg-secondary hover:border-grey-main transition-all duration-300"
+						onClick={() => setCount(count + 1)}
+					>
+						Increment
+					</button>
+					<button
+						className="text-md border rounded-lg py-1 px-2 hover:bg-bg-secondary hover:border-grey-main transition-all duration-300"
+						onClick={() => setCount(count - 1)}
+					>
+						Decrement
+					</button>
+				</div>
+			</div>
+			<Image
+				src={'/react-useState.svg'}
+				alt={''}
+				width={700}
+				height={300}
+			></Image>
+		</div>
+	);
+};
+
+export default Counter;
